939813e0af0729b324505ddda9d529299fd6f31c
DaisukeMiyazaki <aubozumom@icloud.com>
2024-08-11 16:52:34 +0900

    feat: implemented thumbnail generation

--- scripts/convert_images_to_thumbnails_and_originals.sh
+++ scripts/convert_images_to_thumbnails_and_originals.sh
@@ -1,3 +1,35 @@
-# /bin/bash
+#!/bin/bash
 # Run this script to convert the images under /images/pre_published/ to 
 # images/thumbnails and move the pre_published images to /images/original
+
+# Usage: ./scripts/convert_images_to_thumbnails_and_originals.sh ./images/pre_published/ ./images/thumbnail/
+# this script requires imagemagick to be installed.
+# https://imagemagick.org/
+
+readonly IMAGE_DIR=${1}
+echo "IMAGE_DIR: $IMAGE_DIR"
+
+readonly THUMBNAIL_DIR=${2}
+echo "THUMBNAIL_DIR: $THUMBNAIL_DIR"
+
+THUMBNAIL_SIZE="200x200"
+
+# compression quality
+QUALITY=100
+
+
+for file in "$IMAGE_DIR"*; do
+    echo "Processing file: $file"
+    extension="${file##*.}"
+    echo "extension: $extension"
+
+    # thumbnail file name is the same as the original file name
+    thumbnail_file="${THUMBNAIL_DIR}${file##*/}"
+
+    # generate thumbnail here
+    magick "$file" -resize "$THUMBNAIL_SIZE" -quality $QUALITY "$thumbnail_file"
+
+    echo "Created thumbnail: $thumbnail_file"
+done
+
+echo "Thumbnail creation complete."
