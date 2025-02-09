6ab62af823740c603055a3a78046f935ef16b733
Daisuke Miyazaki <daisuke.miyazaki012@gmail.com>
2025-02-09 12:41:06 +0900

    feat: 写真の尺を調整

--- scripts/generate_static_html.py
+++ scripts/generate_static_html.py
@@ -18,7 +18,7 @@ html_template = """<!DOCTYPE html>
       <h1 class="text-center text-3xl font-bold text-white">Dream House Project</h1>
       <a href="./about.html" class="text-center text-white transition duration-300 ease-in-out hover:underline hover:scale-105">このサイトについて</a>
     </div>
-    <div class="flex flex-wrap justify-center gap-4">
+    <div class="flex flex-wrap justify-center gap-4 items-start">
       %IMAGE_TAGS%
     </div>
     <a href="./about.html" class="text-center block text-white transition duration-300 ease-in-out hover:underline hover:scale-105">このサイトについて</a>
@@ -32,7 +32,7 @@ html_template = """<!DOCTYPE html>
 image_tags = ""
 for filename in os.listdir(thumbnail_dir):
     name, extension = os.path.splitext(filename)
-    image_tags += f"<img src=\"./images/thumbnail/{filename}\" alt=\"{name}\" class=\"w-full lg:w-1/3 rounded shadow-md\">\n"
+    image_tags += f"<img src=\"./images/thumbnail/{filename}\" alt=\"{name}\" class=\"w-full lg:w-1/3 h-auto rounded shadow-md\">\n"
 
 # replace the %IMAGE_TAGS% placeholder with the actual image tags
 html = html_template.replace("%IMAGE_TAGS%", image_tags)
